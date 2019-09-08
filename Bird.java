f34cb0ce697cdf9d19497d3ccef7fafab95398c6
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2019-09-08 17:33:07 +0200

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,15 @@
+public class Bird extends Animal {
+
+	@Override
+	void move() {
+		System.out.println("Moves by flying.");
+
+	}
+
+	@Override
+	void eat() {
+		System.out.println("Eats birdfood.");
+
+	}
+
+}
