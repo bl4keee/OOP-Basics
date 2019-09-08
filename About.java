2c3fcbaa9bd8882b9eec135b520bb7aae7525572
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2019-09-08 17:42:49 +0200

    Add files via upload

---
+++ About.java
@@ -0,0 +1,10 @@
+
+public class About {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is just a folder with basic java exercises based on fundamental oop concepts.");
+
+	}
+
+}
